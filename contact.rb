9d444b51b35b7c4f16de78bd7677dbb6d0052695
sylvia villegas <sylvia@svillegas.com>
2017-01-21 17:02:01 -0500

    Add material from lecture; complete class methods

--- contact.rb
+++ contact.rb
@@ -1,28 +1,67 @@
 class Contact
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
+
+  @@contacts = []
+  @@next_id = 1000
+
+  # Call the initializer, store the newly created contact, and then return it
+  def self.create(first_name, last_name, email, note)
+    new_contact = self.new(first_name,last_name, email, note)
+    @@contacts << new_contact
+    new_contact
+  end
+
+  # Return all of the existing contacts
+  def self.all
+    @@contacts
+  end
+
+  # Accept an id as an argument and return the contact who has that id
+  def self.find(search_id)
+    @@contacts.each do |contact|
+      return contact if contact.id == search_id
+    end
+  end
+
+  # Search for a contact using attributes other than id
+  # by specifying both the name of the attribute and the value
+  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
+  def self.find_by(attribute, value)
+    @@contacts.each do |contact|
+      return contact if contact.send(attribute) == value
+    end
+  end
+
+  # This method should delete all of the contacts
+  def self.delete_all
+    puts "WARNING: This will delete all of your contacts permanently. This action cannot be undone. Type 'delete' to confirm this action."
+    response = gets.chomp
+    if response == "delete"
+      @@contacts = []
+      puts "Your contact list is now empty."
+    else
+      puts "Your contacts have not been deleted."
+    end
+  end
+
 
   # This method should initialize the contact's attributes
-  def initialize
-
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@next_id
+    @@next_id += 1
   end
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
-
+  # Get contact's full name
+  def full_name
+    full_name = "#{first_name} #{last_name}"
   end
 
-  # This method should return all of the existing contacts
-  def self.all
-
-  end
-
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find
-
-  end
-
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -30,22 +69,6 @@ class Contact
 
   end
 
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
-  end
-
-  # This method should delete all of the contacts
-  def self.delete_all
-
-  end
-
-  def full_name
-
-  end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
@@ -54,5 +77,11 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
+
+
+# TEST
+jose = Contact.create("Jose", "Bautista", "joeybats19@fake.email", "Flippin' bats for your pleasure!")
+edwin = Contact.create("Edwin", "Encarnacion", "edwin@fake.email", "Walking the parrot...in Cleveland")
+pillar = Contact.create("Kevin", "Pillar", "kevin.pillar@fake.email", "He can catch anything!")
